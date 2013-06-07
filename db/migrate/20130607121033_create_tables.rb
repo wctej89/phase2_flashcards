82ea31625c565ea02e79ce96edb8e258ea4e83a8
Apprentice <apprentice@dbc03.local>
2013-06-07 13:52:27 -0700

    update password column name

--- db/migrate/20130607121033_create_tables.rb
+++ db/migrate/20130607121033_create_tables.rb
@@ -1,7 +1,7 @@
 class CreateTables < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string     :email, :password
+      t.string     :email, :password_hash
       
       t.timestamps
     end
