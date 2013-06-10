b37cf4899b0eb13c938629e47b3daaa071e4c764
dbc-warrior <apprentice@devbootcamp.com>
2013-06-09 17:22:16 -0700

    Refactored questions with AJAX

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,6 +1,6 @@
 get '/deck/:deck_name/new_round' do |deck_name|
-  deck = Deck.find_by_name(deck_name)
-  @round = Round.create(user_id: session[:user_id], deck_id: deck.id )
+  @deck = Deck.find_by_name(deck_name)
+  @round = Round.create(user_id: session[:user_id], deck_id: @deck.id )
   redirect "/deck/#{deck_name}/1"
 end
 
@@ -11,27 +11,43 @@ get '/deck/:deck_name/:card_id' do |deck_name, card_id|
   erb :card
 end
 
-post '/deck/:deck_name/:card_id' do |deck_name, card_id|
+# post '/deck/:deck_name/:card_id' do |deck_name, card_id|
+#   @deck = Deck.find_by_name(deck_name)
+#   @card = Card.find(card_id)
+
+#   user = current_user
+#   @round = user.rounds.last
+#   @guess = Guess.find_or_create_by_round_id_and_card_id(@round.id, card_id)
+#   @guess.increment_guesses
+# end
+
+post '/next/deck/:deck_name/:card_id' do |deck_name, card_id|
+  content_type :json
+
   @deck = Deck.find_by_name(deck_name)
   @card = Card.find(card_id)
-
   user = current_user
   @round = user.rounds.last
-  @guess = Guess.find_or_create_by_round_id_and_card_id(@round.id, card_id)
-  @guess.increment_guesses
-
-  if @card.correct?(params[:answer]) 
-    session[:verify] = "Yes, your answer was correct!"
-    @guess.mark_correct
-  else 
-    session[:verify] = "No, your answer was incorrect. Correct answer was #{@card.answer}."
-  end
 
   if @deck.finished?
     @deck.reset
-    redirect "/round/#{@round.id}"
+    url = "/round/#{@round.id}"
   else
     @card = @deck.find_next
-    redirect to("/deck/#{deck_name}/#{@card.id}")
+    url = "/deck/#{deck_name}/#{@card.id}"
   end
+  url.to_json
 end
+
+post '/deck/:deck_name/:card_id/:user_answer' do |deck_name, card_id, user_answer|
+  @deck = Deck.find_by_name(deck_name)
+  @card = Card.find(card_id)
+  user = current_user
+  @round = user.rounds.last
+  
+  @guess = Guess.find_or_create_by_round_id_and_card_id(@round.id, card_id)
+  @guess.increment_guesses
+  @guess.mark_correct if @card.correct?(user_answer) 
+  @card.answer
+end
+
---
+++ public/js/quiz.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  $('#card').on('submit', function(e) {
+    e.preventDefault();
+    user_answer = $('#card input').val();
+    path = $('#card').attr('action')+'/'+user_answer;
+    next_path = '/next'+ $('#card').attr('action');
+    $("#next").fadeIn(500);
+    send_answer();
+  });
+
+  var redirect = function(data){
+    $.ajax({
+      type: "POST",
+      url: next_path
+      }).done(function(location){
+        window.location = location;
+    });
+  }
+
+  var button_for_next = function(data){
+    $("#next_card").on('click', function () {
+      $("#next").fadeOut(200);
+      redirect(data); 
+    });
+  }
+
+  var send_answer = function() {
+    var response = $.ajax({
+      type: "POST",
+      url: path
+    });
+    response.done(function(data) {    
+      $('#answer').html(data);
+      button_for_next(data);
+
+    });
+  } 
+
+});
