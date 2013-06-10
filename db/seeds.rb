a7e257866baa74af061a83bd6ef4afc0865aeda9
dbc-warrior <apprentice@devbootcamp.com>
2013-06-09 21:04:53 -0700

    added decks to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-deck = Deck.create(name: "capitals")
+deck = Deck.create(name: "U.S. Capitals")
 
 capitals = {alabama: "Montgomery",
 alaska: "Juneau",
@@ -16,6 +16,87 @@ capitals.each do |state, capital|
   question = "What is the capital of " + state.to_s.capitalize + "?"
   deck.cards << Card.create(question: question, answer: capital)
 end
+
+deck2 = Deck.create(name: "Country Capitals")
+
+country = {afghanistan: "Kabul",
+austria: "Vienna",
+china: "Beijing",
+egypt: "Cairo",
+finland: "Helsinki",
+slovenia: "Ljubljana"}
+
+country.each do |state, capital|
+
+  question = "What is the capital of " + state.to_s.capitalize + "?"
+  deck2.cards << Card.create(question: question, answer: capital)
+end
+
+deck3 = Deck.create(name: "Periodic Table")
+
+periodic = {h: "hydrogen",
+k: "potassium",
+ca: "calcium",
+ar: "argon",
+cd: "cadmium"}
+
+periodic.each do |state, capital|
+
+  question = "What element is " + state.to_s.capitalize + "?"
+  deck3.cards << Card.create(question: question, answer: capital)
+end
+
+deck3 = Deck.create(name: "U.S. Presidents")
+
+presidents = {first: "George Washington",
+second: "John Adams",
+third: "Thomas Jefferson",
+fourth: "James Madison",
+fifth: "James Monroe"}
+
+presidents.each do |state, capital|
+
+  question = "Who was the " + state.to_s + " president?"
+  deck3.cards << Card.create(question: question, answer: capital)
+end
+
+deck4 = Deck.create(name: "Planets")
+
+planets = {first: "Mercury",
+second: "Venus",
+third: "Earth",
+fourth: "Mars",
+fifth: "Jupiter"}
+
+planets.each do |state, capital|
+
+  question = "What is the " + state.to_s + " planet from the sun?"
+  deck4.cards << Card.create(question: question, answer: capital)
+end
+
+deck5 = Deck.create(name: "Wildcard")
+
+wild = {"What rapper was part of NWA & Westside Connection?" => "Ice Cube",
+"Who famously said 'That really cleared things up for me'?" => 'Phillip',
+"What is another name for assessment day" => "Happy fun day",
+"Who is better Zee or Jeffrey?" => "they are both awesome"}
+
+wild.each do |state, capital|
+
+  question = state
+  deck5.cards << Card.create(question: question, answer: capital)
+end
+
+
+
+
+
+
+
+
+
+
+
 
 # Hawaii: Honolulu,
 # Idaho: Boise,
